90ecf4f385627e47d5c5186fb566cffcb17a3ce3
emanao <emanao@me.com>
2021-06-15 10:15:11 -0400

    Add routes to trip

--- travel-planner-backend/app/controllers/trips_controller.rb
+++ travel-planner-backend/app/controllers/trips_controller.rb
@@ -1,2 +1,16 @@
 class TripsController < ApplicationController
+    def index
+        all = Trip.all
+        render all.to_json(:except=>[:created_at, :updated_at])
+    end
+    def create 
+
+    end
+    def show
+
+    end
+    private
+    def trip_params
+        params.require(:trip).permit(:name, :notes)
+    end
 end
